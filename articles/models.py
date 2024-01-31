9e928a84d4e412792c127d7ce18cbbc140e8a36c
mtsparrow11 <mtsparrow11@gmail.com>
2024-01-31 14:05:56 +0330

    create project

---
+++ articles/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+    title = models.CharField(max_length=80)
+    slug = models.SlugField()
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    #thumbnail
+    #author
+    
+    def __str__(self):
+        return self.title
+    def snippet(self):
+        return self.body[:50] + '...'
+    
+    
+    
+    
